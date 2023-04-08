c18831d3bb1105013c1d671fa8d33e95b46697f5
Vallyblink <alabama.toj@gmail.com>
2023-04-08 22:38:01 +0300

    last comit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+import {onCountryListMarkup} from './js/markup'
+import {onCountryCardMarkup} from './js/markup'
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const refs = { 
+  inputField: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryCard: document.querySelector('.country-info'),}
+
+
+refs.inputField.addEventListener('input', debounce(onSearchCountry, 300))
+function onSearchCountry(el){
+  el.preventDefault();
+  const countryInput = el.target.value.trim();
+  if (!countryInput) {
+    resetList();
+    return;
+  }
+
+  fetchCountries(countryInput)
+    .then(data => {
+      console.log(data)
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length > 1) {
+        resetList()
+        const markupCountryList = onCountryListMarkup(data);
+        refs.countryList.innerHTML = markupCountryList;
+      } else {
+        resetList()
+        const markupCountryInfo = onCountryCardMarkup(data);
+        refs.countryCard.innerHTML = markupCountryInfo;
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      return;
+    });
+};
+
+function resetList() {
+  refs.countryList.innerHTML = '';
+  refs.countryCard.innerHTML = '';
+}
+
+
---
+++ src/js/markup.js
@@ -0,0 +1,22 @@
+export function onCountryListMarkup(countries){
+     return countries.map(({name,flags,}) =>
+     `<li class='country-list'>
+          <img src='${flags.svg}' alt='${name.official}' width='30'>
+          <p>${name.official}</p>
+        </li>`
+     ).join('');
+}
+
+export function onCountryCardMarkup(countries){
+    return countries.map(({name, capital, population, languages, flags })=>
+    `<h1 class='country-title'>
+        <img class = 'country-flag' src='${flags.svg}' 
+        alt='${name.official}'>
+        <p>${name.official}</p> 
+      </h1> 
+      <ul> 
+        <li class='country-item'>Capital: ${capital}</li> 
+        <li class='country-item'>Population: ${population}</li> 
+        <li class='country-item'>Languages: ${Object.values(languages)}</li> 
+      </ul>`).join('')
+    }
